f3319de185202d90b5c44b5e838918d698bb775f
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-06-22 10:43:19 -0300

    feat: next auth provider added

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma"
-import React, { useState } from 'react'
+import React from 'react'
 
 const getTrips = async () => {
   const trips = await prisma.trip.findMany({})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+"use client"
+
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const {data} = useSession()
+
   return (
-    <h1>Home</h1>
+    <div>
+      <div className="flex flex-col">
+        <button onClick={() => signIn()}>Login</button>
+        <button onClick={() => signOut()}>Logout</button>
+      </div>
+      <h1>Olá, {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? ""}/>
+    </div>
   );
 }
